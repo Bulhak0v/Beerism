06ccd71712b8f3f4128ac9a10d4c7d5c2d80ce2e
Bulhak0v <124911035+SerhijBohatyr004@users.noreply.github.com>
2025-11-06 17:32:17 +0200

    Deployment issues fix #1
    Attempting to fix issues with deployment of the project on vercel #1

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,11 +1,7 @@
 ﻿import app from "./app.js";
-import { LocationsService } from "./services/locations.service.js";
 
 console.log(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 4000;
-
-const list = LocationsService.getAll();
-console.log(list);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
