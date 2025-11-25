8e7bc4ee552f61108f0f37c0bcf21b4847e05146
Bulhak0v <144682616+Bulhak0v@users.noreply.github.com>
2025-11-25 20:17:02 +0200

    Update locations_pg.tsx

--- apps/web/src/pages/locations_pg.tsx
+++ apps/web/src/pages/locations_pg.tsx
@@ -33,10 +33,16 @@ const LocationsPage: React.FC = () => {
         city = encodeURIComponent(searchTerm);
       }
 
+      console.log(city)
+      console.log(user!.user_id)
+
       const res = await fetch(`https://beerism-backend.onrender.com/api/locations/recommendations/byCity?user_id=${user!.user_id}&city=${city}}`);
       if (!res.ok) {
         throw new Error('Failed to fetch locations');
       }
+
+      console.log(res);
+      
       const data = await res.json();
       setLocations(data);
       setCurrentPage(1); 
