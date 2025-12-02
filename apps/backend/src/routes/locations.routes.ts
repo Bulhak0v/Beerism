3f250e298946ed2e4e8cbcf903c76219c04402af
Just2Slow <just200slow@gmail.com>
2025-12-03 00:15:46 +0200

    More backend fixes

--- apps/backend/src/routes/locations.routes.ts
+++ apps/backend/src/routes/locations.routes.ts
@@ -3,13 +3,13 @@ import { getLocations, deleteLocation, addLocation, updateLocation, getLocationB
 
 const router = Router();
 
+router.get("/recommendations/byCity", getRecommendedLocations);
+router.get("/cities", getAllCities);
+router.get("/options", getFilterOptions);
 router.get("/", getLocations);
 router.delete("/:id", deleteLocation);
 router.post("/", addLocation);
 router.put("/:id", updateLocation);
-router.get("/recommendations/byCity", getRecommendedLocations);
-router.get("/cities", getAllCities);
 router.get("/:id", getLocationById);
-router.get("/options", getFilterOptions);
 
 export default router;
