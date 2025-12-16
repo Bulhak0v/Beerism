3f50ac89f8eefb515bea86dd0c0c9891705344ca
Just2Slow <just200slow@gmail.com>
2025-12-16 20:50:00 +0200

    Forgot password fix #4

--- apps/backend/src/config/mailer.ts
+++ apps/backend/src/config/mailer.ts
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
-    user: "yeahiwritestuff@gmail.com",
-    pass: "edzkjaokdjhskhay",
+    type: 'OAuth2',
+    user: process.env.SMTP_USER,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
   },
 });
 
